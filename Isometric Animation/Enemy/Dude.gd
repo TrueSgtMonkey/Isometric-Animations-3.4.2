02829a9a7d7800a2d2f99ec0801babbddc888499
Cody Thompson <47369629+TrueSgtMonkey@users.noreply.github.com>
2022-01-17 16:58:45 -0800

    Revert "end of project with gdscript only"
    This reverts commit d6f880e9e93a7be14ff5ba524015df3d6822bb4f.

--- Isometric Animation/Enemy/Dude.gd
+++ Isometric Animation/Enemy/Dude.gd
@@ -2,17 +2,14 @@ extends KinematicBody
 
 export (float) var speed = 3.0
 
-var velocity := Vector3()
-var gravity := 30.0
 var frame_num := 0
 var curr_angle := 0
-var last_frame : int
-
 var frame_timer := Timer.new()
 var move_timer := Timer.new()
+var velocity := Vector3()
+var gravity := 30.0
 
 func _ready():
-	last_frame = $Sprite3D.hframes * $Sprite3D.vframes
 	frame_timer.one_shot = false
 	frame_timer.wait_time = 1.0 / 24.0
 	frame_timer.connect("timeout", self, "increase_frame")
@@ -62,47 +59,14 @@ func getCurrAngle(vel : Vector3):
 	
 # plays the sprite sheet animation one frame at a time in one dimension
 func animation(st_frame : int, max_frame : int):
-	#play animation normally
-	if st_frame < max_frame:
-		frame_num += 1
-		if frame_num > last_frame:
-			printerr("out of bounds of $Sprite3D.hframes * $Sprite3D.vframes in ", name)
-		if frame_num >= max_frame && frame_num <= last_frame:
-			frame_num = st_frame
-	elif st_frame > max_frame:
-		frame_num -= 1
-		if frame_num < 0:
-			printerr("out of bounds (frame_num < 0) in ", name)
-		if frame_num <= max_frame && frame_num >= 0:
-			frame_num = st_frame
-		
+	pass
 			
-# angle: current direction the sprite is facing (CCW).
+# angle: current direction the sprite is facing.
 # st_frame: where we want our animation to start from in case there is 
 #	 another animation - starts at 0 by default
-func iso_animation(angle : int, st_angle : int = 0):
-	if st_angle < 0:
-		printerr("st_angle cannot be negative in ", name)
-		return
-	var curr_frame : int = frame_num % $Sprite3D.hframes
-	frame_num = curr_frame + (angle * $Sprite3D.hframes)
-	frame_num += st_angle * $Sprite3D.hframes
-	
-	animation((st_angle + angle) * $Sprite3D.hframes, (st_angle + angle + 1) * $Sprite3D.hframes - 1)
-	
-#	match angle:
-#		0: #east
-#			animation(st_angle * $Sprite3D.hframes, (st_angle + 1) * $Sprite3D.hframes - 1)
-#		1: #northeast
-#			animation((st_angle + 1) * $Sprite3D.hframes, (st_angle + 2) * $Sprite3D.hframes - 1)
-#		2: #north
-#		3: #northwest
-#		4: #west
-#		5: #southwest
-#		6: #south
-#		7: #southeast
+func iso_animation(angle : int, st_frame : int = 0):
+	pass
 
 # called by a timer
 func increase_frame():
-	iso_animation(curr_angle, 0)
-	$Sprite3D.frame = frame_num
+	pass
